ebe1568a7cfe060a4150dd615499c6ab98691287
Olivier Lemoine <olivier@le-moine.fr>
2021-07-08 13:44:04 +0200

    base algebra

---
+++ src/vec2.rs
@@ -0,0 +1,48 @@
+use std::ops::Add;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vec2 {
+    pub x: f32,
+    pub y: f32,
+}
+impl Vec2 {
+    pub fn from_polar_deg(mag: f32, angle_deg: f32) -> Self {
+        Self::from_polar_rad(mag, angle_deg.to_radians())
+    }
+    pub fn from_polar_rad(mag: f32, angle_rad: f32) -> Self {
+        Vec2 {
+            x: mag * angle_rad.cos(),
+            y: mag * angle_rad.sin(),
+        }
+    }
+    pub fn rot_deg(&self, deg: f32) -> Self {
+        self.rot_rad(deg.to_radians())
+    }
+    pub fn rot_rad(&self, rad: f32) -> Self {
+        Vec2 {
+            x: rad.cos() * self.x + rad.sin() * self.y,
+            y: rad.sin() * self.x - rad.cos() * self.y,
+        }
+    }
+}
+
+impl From<(f32, f32)> for Vec2 {
+    fn from((x, y): (f32, f32)) -> Self {
+        Vec2 { x, y }
+    }
+}
+
+impl From<[f32; 2]> for Vec2 {
+    fn from([x, y]: [f32; 2]) -> Self {
+        Vec2 { x, y }
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Self;
+    fn add(mut self, rhs: Vec2) -> Self::Output {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self
+    }
+}
