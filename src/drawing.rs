2f997ffed9d6f5925cb260f502f18e707b912ae3
Olivier Lemoine <olivier@le-moine.fr>
2021-07-10 09:22:27 +0200

    add canvas anchor

--- src/drawing.rs
+++ src/drawing.rs
@@ -5,18 +5,24 @@ pub type Size = Vec2;
 
 #[derive(Debug)]
 pub struct Drawing {
-    canvas_size: Size,
+    pub canvas_size: Size,
+    pub canvas_anchor: Size,
     shapes: Vec<Shape>,
 }
 impl Drawing {
     pub const fn empty() -> Self {
         Drawing {
             canvas_size: Vec2::from_cartesian_tuple((0., 0.)),
+            canvas_anchor: Vec2::from_cartesian_tuple((0., 0.)),
             shapes: vec![],
         }
     }
     pub const fn with_canvas_size(mut self, canvas_size: (f32, f32)) -> Self {
         self.canvas_size = Vec2::from_cartesian_tuple(canvas_size);
+        self
+    }
+    pub const fn with_canvas_anchor(mut self, canvas_anchor: (f32, f32)) -> Self {
+        self.canvas_anchor = Vec2::from_cartesian_tuple(canvas_anchor);
         self
     }
     pub fn add(&mut self, shape: Shape) {
