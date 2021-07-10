09db6909f878d3c8651c2aba443ef6d2f5126e13
Olivier Lemoine <olivier@le-moine.fr>
2021-07-10 08:36:44 +0200

    add canvas size

--- src/drawing.rs
+++ src/drawing.rs
@@ -1,12 +1,23 @@
 use crate::shapes::Shape;
+use algebra::Vec2;
+
+pub type Size = Vec2;
 
 #[derive(Debug)]
 pub struct Drawing {
+    canvas_size: Size,
     shapes: Vec<Shape>,
 }
 impl Drawing {
-    pub fn empty() -> Self {
-        Drawing { shapes: vec![] }
+    pub const fn empty() -> Self {
+        Drawing {
+            canvas_size: Vec2::from_cartesian_tuple((0., 0.)),
+            shapes: vec![],
+        }
+    }
+    pub const fn with_canvas_size(mut self, canvas_size: (f32, f32)) -> Self {
+        self.canvas_size = Vec2::from_cartesian_tuple(canvas_size);
+        self
     }
     pub fn add(&mut self, shape: Shape) {
         self.shapes.push(shape);
