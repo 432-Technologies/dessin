cc42452db2954998fc5cee230dd2add70eb86959
Olivier Lemoine <olivier@le-moine.fr>
2021-07-20 17:03:33 +0200

    update import

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
 mod drawing;
 mod position;
 mod shapes;
-mod style;
+pub mod style;
 
 pub type Size = Vec2;
 
 pub use crate::drawing::{AddShape, Drawing};
+pub use crate::position::Rect;
+pub use crate::shapes::{Shape, ShapeType};
 pub use algebra::{vec2, Angle, Vec2};
+
 pub mod shape {
     pub use crate::shapes::arc::Arc;
     pub use crate::shapes::circle::Circle;
     pub use crate::shapes::image::{Image, ImageFormat};
     pub use crate::shapes::line::{Line, LineBuilder};
     pub use crate::shapes::text::Text;
+    pub use crate::style::{Color, Fill, Stroke, Style};
 }
