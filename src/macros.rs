b35df92c9e09c7c7e011f4822619190e337cf921
Olivier Lemoine <olivier@le-moine.fr>
2021-07-21 16:23:26 +0200

    update macros scrope

--- src/macros.rs
+++ src/macros.rs
@@ -1,8 +1,8 @@
 #[macro_export]
-macro_rules! impl_pos {
+macro_rules! impl_pos_at {
     ($t:ty) => {
         impl $t {
-            pub const fn with_pos(mut self, pos: crate::position::Rect) -> Self {
+            pub const fn with_pos(mut self, pos: $crate::position::Rect) -> Self {
                 self.pos = pos;
                 self
             }
@@ -11,12 +11,26 @@ macro_rules! impl_pos {
                 self.pos = self.pos.at(pos);
                 self
             }
+        }
+    };
+}
 
+#[macro_export]
+macro_rules! impl_pos_anchor {
+    ($t:ty) => {
+        impl $t {
             pub const fn with_anchor(mut self, anchor: ::algebr::Vec2) -> Self {
                 self.pos = self.pos.with_anchor(anchor);
                 self
             }
+        }
+    };
+}
 
+#[macro_export]
+macro_rules! impl_pos_size {
+    ($t:ty) => {
+        impl $t {
             pub const fn with_size(mut self, size: ::algebr::Vec2) -> Self {
                 self.pos = self.pos.with_size(size);
                 self
@@ -26,15 +40,24 @@ macro_rules! impl_pos {
 }
 
 #[macro_export]
+macro_rules! impl_pos {
+    ($t:ty) => {
+        $crate::impl_pos_at!($t);
+        $crate::impl_pos_anchor!($t);
+        $crate::impl_pos_size!($t);
+    };
+}
+
+#[macro_export]
 macro_rules! impl_style {
     ($t:ty) => {
         impl $t {
-            pub const fn with_style(mut self, style: crate::style::Style) -> Self {
+            pub const fn with_style(mut self, style: $crate::style::Style) -> Self {
                 self.style = Some(style);
                 self
             }
 
-            pub fn with_stroke(mut self, stroke: crate::style::Stroke) -> Self {
+            pub fn with_stroke(mut self, stroke: $crate::style::Stroke) -> Self {
                 self.style = {
                     let mut style = self.style.unwrap_or_default();
                     style.stroke = Some(stroke);
@@ -43,7 +66,7 @@ macro_rules! impl_style {
                 self
             }
 
-            pub fn with_fill(mut self, fill: crate::style::Fill) -> Self {
+            pub fn with_fill(mut self, fill: $crate::style::Fill) -> Self {
                 self.style = {
                     let mut style = self.style.unwrap_or_default();
                     style.fill = Some(fill);
