fee2ac421e4f62fe0bfd7208389f54df88c139b3
OlivierLemoine <16369060+OlivierLemoine@users.noreply.github.com>
2021-07-29 13:53:16 +0200

    OlivierLemoine/issue28 (#37)

--- dessin/src/style.rs
+++ dessin/src/style.rs
@@ -1,9 +1,32 @@
 pub use crate::shapes::text::{FontWeight, TextAlign};
 
+pub fn rbg(r: u8, g: u8, b: u8) -> Color {
+    Color::RGB { r, g, b }
+}
+
+pub fn rgba(r: u8, g: u8, b: u8, a: u8) -> Color {
+    Color::RGBA { r, g, b, a }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Color {
+    RGBA { r: u8, g: u8, b: u8, a: u8 },
     RGB { r: u8, g: u8, b: u8 },
     U32(u32),
+}
+impl Color {
+    pub fn rgba(self) -> Color {
+        match self {
+            Color::RGBA { .. } => self,
+            Color::RGB { r, g, b } => Color::RGBA { r, g, b, a: 255 },
+            Color::U32(c) => Color::RGBA {
+                r: ((c >> 16) & 0xFF) as u8,
+                g: ((c >> 8) & 0xFF) as u8,
+                b: (c & 0xFF) as u8,
+                a: 255,
+            },
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
