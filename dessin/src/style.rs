7b6fb1f6f2bd06eb50d799b99b417169925d243e
OlivierLemoine <16369060+OlivierLemoine@users.noreply.github.com>
2021-08-09 11:29:46 +0200

    Add basic color (#49)

--- dessin/src/style.rs
+++ dessin/src/style.rs
@@ -1,10 +1,10 @@
 pub use crate::shapes::text::{FontWeight, TextAlign};
 
-pub fn rbg(r: u8, g: u8, b: u8) -> Color {
+pub const fn rbg(r: u8, g: u8, b: u8) -> Color {
     Color::RGB { r, g, b }
 }
 
-pub fn rgba(r: u8, g: u8, b: u8, a: u8) -> Color {
+pub const fn rgba(r: u8, g: u8, b: u8, a: u8) -> Color {
     Color::RGBA { r, g, b, a }
 }
 
@@ -15,7 +15,21 @@ pub enum Color {
     U32(u32),
 }
 impl Color {
-    pub fn rgba(self) -> Color {
+    pub const RED: Color = rbg(255, 0, 0);
+    pub const GREEN: Color = rbg(0, 255, 0);
+    pub const BLUE: Color = rbg(0, 0, 255);
+    pub const WHITE: Color = rbg(255, 255, 255);
+    pub const BLACK: Color = rbg(0, 0, 0);
+    pub const YELLOW: Color = rbg(255, 255, 0);
+    pub const ORANGE: Color = rbg(255, 165, 0);
+    pub const MAGENTA: Color = rbg(255, 0, 255);
+    pub const CYAN: Color = rbg(0, 255, 255);
+    pub const GRAY: Color = rbg(128, 128, 128);
+    pub const TRANSPARENT: Color = rgba(0, 0, 0, 0);
+    pub const LIGHT_GRAY: Color = rbg(192, 192, 192);
+    pub const DARK_GRAY: Color = rbg(64, 64, 64);
+
+    pub const fn rgba(self) -> Color {
         match self {
             Color::RGBA { .. } => self,
             Color::RGB { r, g, b } => Color::RGBA { r, g, b, a: 255 },
