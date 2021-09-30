3fda1ef0104fc3e2cd181d455988879d281e2f66
Olivier Lemoine <olivier@le-moine.fr>
2021-09-30 11:49:13 +0200

    Fixes tests

--- dessin-svg/src/shapes.rs
+++ dessin-svg/src/shapes.rs
@@ -203,7 +203,7 @@ mod tests {
                 .with_align(TextAlign::Center),
         );
 
-        let text_svg = r#"<text x="-10" y="-10" text-anchor="middle" font-size="10" font-weight="bold" fill='rgba(255,0,0,1)' >hello world</text>"#;
+        let text_svg = r#"<text x="10" y="-10" text-anchor="middle" font-size="10" font-weight="bold" fill='rgba(255,0,0,1)' >hello world</text>"#;
         let drawing_svg = format!(r#"<svg viewBox="-0 -0 0 0">{}</svg>"#, text_svg);
 
         assert_eq!(drawing.shapes()[0].to_svg().unwrap(), text_svg);
@@ -218,7 +218,7 @@ mod tests {
                 .to(vec2(20., 20.))
                 .with_fill(Fill::Color(Color::U32(0xFF0000))),
         );
-        let line_svg = r#"<line x1="10" y1="10" x2="20" y2="20" fill='rgba(255,0,0,1)' />"#;
+        let line_svg = r#"<line x1="10" y1="-10" x2="20" y2="-20" fill='rgba(255,0,0,1)' />"#;
         let drawing_svg = format!(r#"<svg viewBox="-0 -0 0 0">{}</svg>"#, line_svg);
         assert_eq!(drawing.shapes()[0].to_svg().unwrap(), line_svg);
         assert_eq!(drawing.to_svg().unwrap(), drawing_svg);
