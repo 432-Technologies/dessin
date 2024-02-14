5f98d5b8be8a98919f791944656d421a2811f32d
Math/Nuba <quintana.mathieu@yahoo.fr>
2024-02-14 17:58:53 +0100

    correct error in red_circle_without_macro helped by François and Olivier

--- examples/red_circle_without_macro.rs
+++ examples/red_circle_without_macro.rs
@@ -10,16 +10,17 @@ use dessin::{
 
 fn main(){
 
-    /// create a circle with radius of 11
+    // create a circle with radius of 11
     let circle:Shape = Circle::default().with_radius(11.).into();
 
     let mut circle = Style::new(circle);
 
-    /// paints the inside of the circle in red
+    // paints the inside of the circle in red
     circle.fill(Fill::Color(rgb(255, 0, 0)));
 
-    /// creates a grey margin of 0.2 (0.1 outside and 0.1 inside the circle)
-    circle.stroke=(Stroke::Full { color: rgb(0x96, 0x96, 0x96), width: 0.2 });
+    // creates a grey margin of 0.2 (0.1 outside and 0.1 inside the circle)
+    circle.stroke(Stroke::Full { color: rgb(96, 96, 96), width: 0.2 });
     
-    fs::write("./target/432.svg", circle.to_svg().unwrap()).unwrap();  //print in svg version
+    //print in svg version
+    fs::write("./target/432.svg", circle.to_svg().unwrap()).unwrap();  
 }
