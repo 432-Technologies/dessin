eace07271f78950a52cb79f545ab667385547ac3
Math/Nuba <quintana.mathieu@yahoo.fr>
2024-03-07 09:58:47 +0100

    Translate all old macro use to new macro

--- examples/arc_with_macro.rs
+++ examples/arc_with_macro.rs
@@ -4,20 +4,17 @@ use dessin::{nalgebra::Rotation2, prelude::*};
 use project_root::get_project_root;
 
 fn main() {
-    let arc: Shape = dessin!([
-        Arc: #(
-
-        start_angle={0.}
-
-        end_angle={PI/4.}
-
+    let arc: Shape = dessin2!([Arc!(
+        start_angle = 0.,
+        end_angle = PI / 4.,
         // creates a black pointing margin with a width of 0.1
-        stroke={Stroke::Full { color: rgb(0, 50, 75), width: 0.1}}
-
+        stroke = Stroke::Full {
+            color: rgb(0, 50, 75),
+            width: 0.1
+        },
         // chooses a rotation of -10 radians in the trigonometric direction
-        rotate={Rotation2::new(-10_f32.to_radians())}
-    ),
-    ]);
+        rotate = Rotation2::new(-10_f32.to_radians())
+    ),]);
 
     // prints in svg version
     fs::write(
--- examples/blue_triangle_with_macro.rs
+++ examples/blue_triangle_with_macro.rs
@@ -7,23 +7,19 @@ use dessin::{
 use project_root::get_project_root;
 
 fn main() {
-    let triangle: Shape = dessin!([
-         Triangle: #(
-
-         // chooses an equilateral triangle [(x,x) => equilateral] with a size of 5. [if you want an isosceles triangle : (x,y)]
-         scale={Scale2::new(5., 5.)}
-
-         // paints the inside of the triangle in green
-         fill={rgb(0,0,255)}
-
-         // creates a black margin of 0.1 (0.05 outside and the same inside the triangle)
-         stroke={Stroke::Full { color: rgb(0, 0, 0), width: 0.1 }}
-
-         //chooses a rotation of 0 radians in the trigonometric direction
-         rotate={Rotation2::new(0_f32.to_radians())}
-     ),
-
-    ]);
+    let triangle: Shape = dessin2!([Triangle!(
+        // chooses an equilateral triangle [(x,x) => equilateral] with a size of 5. [if you want an isosceles triangle : (x,y)]
+        scale = Scale2::new(5., 5.),
+        // paints the inside of the triangle in green
+        fill = rgb(0, 0, 255),
+        // creates a black margin of 0.1 (0.05 outside and the same inside the triangle)
+        stroke = Stroke::Full {
+            color: rgb(0, 0, 0),
+            width: 0.1
+        },
+        //chooses a rotation of 0 radians in the trigonometric direction
+        rotate = Rotation2::new(0_f32.to_radians())
+    ),]);
 
     // prints in svg version
     fs::write(
